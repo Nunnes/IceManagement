6882905d76b6398f145e7235f526621c5d28e55b
Vasco Conde <vasconde@gmail.com>
2012-02-08 00:00:39 +0000

    remover o push_git.sh

--- tables.sql
+++ tables.sql
@@ -8,21 +8,21 @@ drop table if exists Customer;
 	    email	  	varchar(64)	     
      ) ENGINE=INNODB;
 
-drop table if exists Customer_Purchase;
+drop table if exists Customer_Transaction;
 
-     create table Customer_Purchase (
-     	    purchase_id		 int	primary key	references purchase(id),
+     create table Customer_Transaction (
+     	    transaction_id		 int	primary key	references transaction(id),
      	    customer_id		 int	not null	references Customer(id)
      ) ENGINE=INNODB;
 
-drop table if exists Purchase;
+drop table if exists Transaction;
 
-     create table Purchase (
+     create table Transaction (
      	    id			int		primary key,
 	    amount		int,
 	    price		double		not null,
 	    on_credit		boolean		not null,
- 	    data_purchase	date		not null	    
+ 	    data_transaction	date		not null	    
      ) ENGINE=INNODB;
 
 -- add some info --
@@ -33,14 +33,14 @@ insert into Customer values (2, 'nuno', 'nunes', '919623469', 'nunnes00@gmail.co
 
 insert into Customer values (3, 'gilberto', 'conde', '96534234', 'vitamina.gil@gmail.com');
 
-insert into Purchase values (1, 2, 23.00, false, '2010-03-12');
+insert into Transaction values (1, 2, 23.00, false, '2010-03-12');
 
-insert into Purchase values (2, 100, 3000.00, false, '2010-3-31');
+insert into Transaction values (2, 100, 3000.00, false, '2010-3-31');
 
-insert into Purchase values (3, 32, 234.00, true, '2010-2-21');
+insert into Transaction values (3, 32, 234.00, true, '2010-2-21');
 
-insert into Customer_Purchase values (1,1);
+insert into Customer_Transaction values (1,1);
 
-insert into Customer_Purchase values (3,1);
+insert into Customer_Transaction values (3,1);
 
-insert into Customer_Purchase values (2,3);
+insert into Customer_Transaction values (2,3);
